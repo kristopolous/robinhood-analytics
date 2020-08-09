4d69fe3dbee8be24adea8d53b3cca06ade5e983e
Chris McKenzie <kristopolous@yahoo.com>
2020-08-09 15:12:44 -0700

    cleanup

--- repl.py
+++ repl.py
@@ -8,8 +8,6 @@ import configparser
 import pdb
 import logging
 
-from pprint import pprint
-
 from p import robin
 from p import lib
 from p import db
@@ -19,27 +17,12 @@ config.read('secrets.ini')
 
 lib.upgrade()
 
-"""
-if len(sys.argv) < 2:
-    print("Login username required")
-    sys.exit(0)
-robin.login(sys.argv[1])
-"""
-
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 db.set_log(logger)
 
 robin.config = config['config']
-"""
-robin.login(
-    username=config['config']['user'], 
-    password=config['config']['password'],
-    device_token=config['config']['token']
-)
-
-"""
 
 while True:
     print("> ", end="")
