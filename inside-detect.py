596d795bf690155136460a116dcb4c5a1a7eaba1
Chris McKenzie <kristopolous@yahoo.com>
2020-09-27 19:44:29 -0700

    adding a dummy

---
+++ inside-detect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+# This is purely a research project
+# Don't use it. I'm not responsible.
+#
+# Alright, that's that.
+#
+import configparser
+
+from p import robin
+from p import lib
+from p import db
